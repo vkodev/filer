68b6915485c3d1ac85fbebacf2e55b092d90e4a8
Igor Noskov <igor.noskov87@gmail.com>
2016-03-17 11:29:30 +0600

    package refactoring

--- common/file.go
+++ common/file.go
@@ -1,7 +1,6 @@
 package common
 
 import (
-	"github.com/labstack/echo"
 	"github.com/satori/go.uuid"
 	"io"
 	"path/filepath"
@@ -74,34 +73,4 @@ func (f *File) Ext() string {
 		return filepath.Ext(f.OriginalFilename)
 	}
 	return filepath.Ext(f.OriginalFileURL)
-}
-
-func UploadFileHandler(fileRepository *FileRepository) echo.HandlerFunc {
-	return func(c echo.Context) error {
-		req := c.Request()
-		//store:= c.Form("store")
-		filename := c.Form("filename")
-		file, err := req.FormFile("file")
-		if err != nil {
-			return err
-		}
-
-		if filename == "" {
-			filename = file.Filename
-		}
-
-		src, err := file.Open()
-		if err != nil {
-			return err
-		}
-		defer src.Close()
-
-		f, err := fileRepository.Upload(src, filename)
-
-		if err != nil {
-			return err
-		}
-
-		return c.JSON(200, f)
-	}
 }
